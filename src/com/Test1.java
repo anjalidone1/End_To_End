b51cdacf9c812441036be1875b38501e93f15616
anjalidone1 <anjalidone14@gmail.com>
2026-03-10 18:02:29 +0530

    workspace2

---
+++ src/com/Test1.java
@@ -0,0 +1,13 @@
+package com;
+
+import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("TC1");
+		System.out.println("TC2");
+		System.out.println("TC3");
+	}
+
+}
